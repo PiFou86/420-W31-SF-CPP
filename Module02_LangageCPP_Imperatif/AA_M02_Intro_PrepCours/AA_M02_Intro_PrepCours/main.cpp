46ba637226b3f5be578fa0156b0621ddca1a5cb2
Pierre-Francois Leon <pierrefrancois.leon@gmail.com>
2023-10-05 11:46:47 -0400

    Correction corruption de projet démo

--- Module02_LangageCPP_Imperatif/AA_M02_Intro_PrepCours/AA_M02_Intro_PrepCours/main.cpp
+++ Module02_LangageCPP_Imperatif/AA_M02_Intro_PrepCours/AA_M02_Intro_PrepCours/main.cpp
@@ -4,5 +4,5 @@
 
 int main()
 {
-    std::cout << min(23, 42) << "Hello World!\n";
+    std::cout << min(23, 42) << std::endl << "Hello World!\n";
 }
--- Module02_LangageCPP_Imperatif/AA_M02_Intro_PrepCours/AA_M02_Intro_PrepCours/util.h
+++ Module02_LangageCPP_Imperatif/AA_M02_Intro_PrepCours/AA_M02_Intro_PrepCours/util.h
@@ -1,7 +1,3 @@
-#include <iostream>
+#pragma once
 
-#include "util.h"
-
-int min(int p_v1, int p_v2) {
-	return p_v1;
-}
+int min(int p_v1, int p_v2);
