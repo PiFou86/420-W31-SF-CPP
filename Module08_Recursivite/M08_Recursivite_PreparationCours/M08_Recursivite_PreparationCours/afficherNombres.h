02ea253913ef77c900517f99f0cb367b0a567a44
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2025-01-30 10:30:16 -0500

    Update afficherNombres.h

--- Module08_Recursivite/M08_Recursivite_PreparationCours/M08_Recursivite_PreparationCours/afficherNombres.h
+++ Module08_Recursivite/M08_Recursivite_PreparationCours/M08_Recursivite_PreparationCours/afficherNombres.h
@@ -8,4 +8,4 @@ void afficherNombresDe1AN_v2(int p_n);
 
 void afficherNombresDe1AN_v3(int p_n);
 
-std::string dureeLisibleParHumain(long long p_ns, bool p_ignoreNs = false, bool P_ignoreUs = false, bool p_ignoreMs = false);
+std::string dureeLisibleParHumain(long long p_ns, bool p_ignoreNs = false, bool p_ignoreUs = false, bool p_ignoreMs = false);
